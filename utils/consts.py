05d004102e6943ed0f7d65ec6423706e96c31f20
Polianok Bogdan <miner34006@gmail.com>
2020-06-02 10:24:45 +0300

    add consts file

---
+++ utils/consts.py
@@ -0,0 +1,10 @@
+class Base():
+    DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+class Time_intervals():
+    HOUR = 'hour'
+    DAY = 'day'
+    INTERVALS_DELTA = {
+        'hour': 7, 
+        'day': 365
+    }
